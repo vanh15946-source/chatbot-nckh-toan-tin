27cafb1ec36c58c3a363ae8bee095c6f60ba38e1
vanh15946-source <vanh15946@gmail.com>
2026-05-05 08:09:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ def load_system():
     embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
     db = Chroma(persist_directory="chroma_db", embedding_function=embedding_model)
 
-    
-llm = ChatGoogleGenerativeAI(
+    # Tải LLM
+    llm = ChatGoogleGenerativeAI(
         model="gemini-1.5-flash",
         google_api_key="AIzaSyDbnskpAfVsA_2YZEx-95prQi05SulIiOA",  # Ví dụ: "AIzaSyB..."
         temperature=0.3  # Độ sáng tạo (0 là bám sát tài liệu nhất)
